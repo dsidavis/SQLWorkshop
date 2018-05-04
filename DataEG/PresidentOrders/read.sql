da2aeb259f9110f30ad36159975012e60ca1e1cd
Duncan Temple Lang <duncan@r-project.org>
2018-05-03 20:23:50 -0700

    removed the headers from the sql tables

--- DataEG/PresidentOrders/read.sql
+++ DataEG/PresidentOrders/read.sql
@@ -1,18 +1,8 @@
 .mode tabs
-.import orders.tsv  orders
+.import orders_nh.tsv  orders
 
-.import customers.csv customer
+.import customers_nh.tsv customer
 
 .tables
-
-select first_name, last_name, order_date, order_amount from customers c left join orders o on c.customer_id = o.customer_id;
-
-select first_name, last_name, order_date, amount from customer c left join orders o on c.customer_id = o.customer_id;
-
-
-select first_name, last_name, order_date, order_amount from customers c right join orders o on c.customer_id = o.customer_id;
-
-
-select first_name, last_name, order_date, order_amount from customers c full join orders o on c.customer_id = o.customer_id;
 
 
