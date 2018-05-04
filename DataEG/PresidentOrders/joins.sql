d7b59eb82421b140e46d5434a0c903f63d9e580d
Duncan Temple Lang <duncan@r-project.org>
2018-05-03 20:18:03 -0700

    ...

---
+++ DataEG/PresidentOrders/joins.sql
@@ -0,0 +1,11 @@
+select first_name, last_name, order_date, order_amount from customers c left join orders o on c.customer_id = o.customer_id;
+
+select first_name, last_name, order_date, amount from customer c left join orders o on c.customer_id = o.customer_id;
+
+
+select first_name, last_name, order_date, order_amount from customers c right join orders o on c.customer_id = o.customer_id;
+
+
+select first_name, last_name, order_date, order_amount from customers c full join orders o on c.customer_id = o.customer_id;
+
+
